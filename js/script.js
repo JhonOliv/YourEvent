5d464e58b676debf085928d1b45c0c43782dee71
Jhonatan Silva <jhonatan.jsilvajos@gmail.com>
2026-04-05 00:34:23 -0300

    Criação da logica de validação do certificado emitido

--- js/script.js
+++ js/script.js
@@ -18,31 +18,32 @@ elements.forEach(el => {
     observer.observe(el);
 });
 
+const tabelaLogin = {
+    dados: [
+        {
+            login: "jhonsilva",
+            senha: "1234@"
+        }
+    ],
+    certificados: [
+        {
+            codigo: 87646333,
+            nomeParticipante: "Kevin de Bruyne",
+            dataEvento: "2026-05-10"
+        }
+    ]
+
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.includes("login")) {
-        const tabelaLogin = {
-            dados: [
-                {
-                    login: "jhonsilva",
-                    senha: "1234@"
-                }
-            ],
-
-        }
-
-
         document.querySelector("#enviar").addEventListener("click", function () {
             let login = document.querySelector("#login").value
             let senha = document.querySelector("#senha").value
 
             console.log("OLA MUNDO".toLocaleLowerCase())
             tabelaLogin.dados.forEach((e) => {
-
-                console.log(login)
-                console.log(senha);
-                console.log(e.login);
 
                 if (e.login == login.toLocaleLowerCase() && e.senha == senha.toLocaleLowerCase()) {
                     window.location.href = "http://localhost:5500/veriCerti.html";
@@ -52,6 +53,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
             })
         })
+
+    }
+
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.href.includes('veriCerti')) {
+
+        console.log("Funcionando!")
+        document.querySelector("#validar").addEventListener('click', function () {
+
+            let codigo = document.querySelector("#codigo").value
+            let nomeParticipante = document.querySelector("#nomeParticipante").value
+            let dataEvento = document.querySelector("#data").value;
+            let validar = document.querySelector("#rowValidar");
+
+            console.log(typeof (codigo));
+            console.log(typeof (nomeParticipante));
+            console.log(typeof (dataEvento));
+
+            tabelaLogin.certificados.forEach(function (e) {
+                console.log(e.codigo);
+                console.log(e.codigo == 87646333 ? true : false);
+                console.log(dataEvento)
+                if (e.codigo == parseInt(codigo) &&
+                    e.nomeParticipante.toLocaleLowerCase().trim() == nomeParticipante.toLocaleLowerCase().trim()
+                    && e.dataEvento == dataEvento) {
+                    document.querySelector("#rowValidar").insertAdjacentHTML("beforeend", `
+                            <div class="alert alert-success alert-dismissible"  role="alert">
+                                Certificado validado com sucesso Cod: ${codigo} 
+                               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+
+                            </div>
+`)
+                } else {
+
+                    const html = `
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                Certificado não é valido tente novamente! Cod: ${codigo} 
+                              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+
+                            </div>
+                        `;
+
+                    validar.insertAdjacentHTML('beforeend', html);
+                }
+            })
+        })
+
 
     }
 })
